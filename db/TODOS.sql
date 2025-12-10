69593a0f6c9a31943cd942c4d1136c1e301a41f6
Katzenheld <victoire.egger@mng.ch>
2025-12-10 12:06:24 +0100

    change db structure

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,3 +11,46 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE mastery(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    namen VARCHAR(100) NOT NULL,
+    vorteile VARCHAR(100) NOT NULL,
+    nachteile VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE geldverdienen(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    art VARCHAR(100) NOT NULL,
+    vorteile VARCHAR(100) NOT NULL,
+    nachteile VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE farmlayout(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    zweck VARCHAR(100) NOT NULL,
+    vorteile VARCHAR(250) NOT NULL,
+    nachteile VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE haustiere(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tierart VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE partner(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    vorname VARCHAR(100) NOT NULL,
+    love_gift VARCHAR(250) NOT NULL,
+    hate_gift VARCHAR(20) NOT NULL,
+    geschlecht VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE sozial_leben ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    haustiere_id INT NOT NULL, 
+    partner_id INT NOT NULL, 
+    FOREIGN KEY (haustiere_id) REFERENCES haustiere(id), 
+    FOREIGN KEY (partner_id) REFERENCES partner(id) 
+); 
