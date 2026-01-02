dd331fb50f20fc831a0d5baa5157b11ebd9b3b3e
Katzenheld <victoire.egger@mng.ch>
2026-01-02 21:11:59 +0100

    korrigiert, intersect

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -14,7 +14,7 @@ CREATE TABLE todos (
 
 CREATE TABLE frage_1 (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    namen VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE frage_2 (
@@ -25,11 +25,6 @@ CREATE TABLE frage_2 (
 CREATE TABLE frage_3 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE frage_4 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    tierart VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE frage_5 (
@@ -48,49 +43,51 @@ CREATE TABLE frage_7 (
 );
 
 
-INSERT INTO frage_1 (name) VALUES
-('Alex'),
-('Sam'),
-('Harvey'),
-('Emily'),
-('Leah'),
-('Haley');
+CREATE TABLE frage_4 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tier VARCHAR(100) NOT NULL
+);
 
-INSERT INTO frage_2 (name) VALUES
-('Eliot'),
-('Harvey'),
-('Leah'),
-('Penny'),
-('Maru'),
-('Sebastian'),
-('Haley');
-
-INSERT INTO frage_3 (name) VALUES
-('Shane'),
-('Eliot'),
-('Leah');
-
-INSERT INTO frage_4 (name) VALUES
+INSERT INTO frage_4 (tier) VALUES
 ('Katze'),
 ('Hund');
 
+
+INSERT INTO frage_1 (name) VALUES
+('Alex'), ('Sam'), ('Harvey'), ('Emily'), ('Leah'), ('Haley');
+
+INSERT INTO frage_2 (name) VALUES
+('Elliot'), ('Harvey'), ('Leah'), ('Penny'), ('Maru'), ('Sebastian'), ('Haley');
+
+INSERT INTO frage_3 (name) VALUES
+('Shane'), ('Elliot'), ('Leah');
+
 INSERT INTO frage_5 (name) VALUES
-('Sebastian'),
-('Abigail'),
-('Shane');
+('Sebastian'), ('Abigail'), ('Shane');
 
 INSERT INTO frage_6 (name) VALUES
-('Alex'),
-('Eliot'),
-('Sam'),
-('Haley'),
-('Leah'),
-('Penny'),
-('Emily');
+('Alex'), ('Elliot'), ('Sam'), ('Haley'), ('Leah'), ('Penny'), ('Emily');
 
 INSERT INTO frage_7 (name) VALUES
-('Harvey'),
-('Sebastian'),
-('Shane'),
-('Abigail'),
-('Maru');
+('Harvey'), ('Sebastian'), ('Shane'), ('Abigail'), ('Maru');
+
+
+SELECT name FROM frage_1
+INTERSECT
+SELECT name FROM frage_3
+INTERSECT
+SELECT name FROM frage_2
+INTERSECT
+SELECT name FROM frage_6;
+
+
+SELECT name FROM frage_1
+INTERSECT
+SELECT name FROM frage_2
+INTERSECT
+SELECT name FROM frage_3
+INTERSECT
+SELECT name FROM frage_5
+INTERSECT
+SELECT name FROM frage_7;
+
